5947771a84cb695de1f56e323e0be1d8c318b421
Patrick Haugh <foehammer.glamdring@gmail.com>
2017-08-05 15:06:14 -0400

    Working state.  Will do further live testing and make changes accordingly

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import requests
 
 client = discord.Client()
 pat = re.compile(r'\[\[(.*?)\]\]')
+target = 'https://api.scryfall.com/cards/named?fuzzy={}&format=text'
 
 @client.event
 async def on_ready():
@@ -19,6 +20,10 @@ async def on_message(message):
         return
     cards = pat.findall(message.content)
     for card in cards:
-        await client.send_message(message.channel, card)
+        name = '+'.join(card.split())
+        resp = requests.get(target.format(name))
+        if resp.status_code == 200:
+            await client.send_message(message.channel, resp.content.decode('UTF-8'))
+            await asyncio.sleep(.05)
 
 client.run('MzQzNDM0MDAzNzMyMzY1MzEz.DGeXcw.EyLpdC6uRoS68lLMqfw1-7Gow70')
