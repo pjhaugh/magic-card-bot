a8e824f60c68657dee693cadccb1aadebb053630
Ubuntu <ubuntu@ip-172-31-17-130.ec2.internal>
2017-08-07 02:39:29 +0000

    Added autocomplete functionality to searches that have no match.

--- bot.py
+++ bot.py
@@ -7,6 +7,10 @@ import sys
 client = discord.Client()
 pat = re.compile(r'\[\[(.*?)\]\]')
 target = 'https://api.scryfall.com/cards/named?fuzzy={}&format=text'
+autocomplete = 'https://api.scryfall.com/cards/autocomplete?q={}'
+search = """I don't recognize the name: {}
+Did you mean one of the following?
+{}"""
 
 @client.event
 async def on_ready():
@@ -20,12 +24,21 @@ async def on_message(message):
     if message.author == client.user:
         return
     cards = pat.findall(message.content)
+    failures = []
     for card in cards:
         name = '+'.join(card.split())
         resp = requests.get(target.format(name))
         if resp.status_code == 200:
             await client.send_message(message.channel, resp.content.decode('UTF-8'))
             await asyncio.sleep(.05)
+        else:
+            failures.append(name)
+    for name in failures:
+        resp = requests.get(autocomplete.format(name))
+        if resp.status_code == 200 and resp.json()['data']:
+            await client.send_message(message.channel, search.format(name, '\n'.join(resp.json()['data'])))
+            await asyncio.sleep(.05)
+
 if len(sys.argv) == 2:
     client.run(sys.argv[1])
 else:
