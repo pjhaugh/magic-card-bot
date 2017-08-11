6819fb5238b79264bd9ad78af522ec3f18354ceb
pjhaugh <foehammer.glamdring@gmail.com>
2017-08-10 23:47:49 -0400

    Mana Symbol Inference
    Laying framework to infer mana symbol emoji ids from client instance

--- mana.py
+++ mana.py
@@ -1,11 +1,16 @@
 import re
 import json
 
-with open("mana_symbols.json") as f:
-    mana_symbols = json.loads(f.read())
+class Mana:
 
-mana_symbols = dict((re.escape(k), v) for k, v in mana_symbols.items())
-mana_pat = re.compile("|".join(mana_symbols.keys()))
+    def __init__(self):
+	    with open("mana_symbols.json") as f:
+            self.mana_symbols = json.loads(f.read())
+    
+	def prime(self, client):
+        d = {e.name, e.id for emoji in client.get_all_emoji()}
+		self.mana_symbols = dict((re.escape(k), "<:{}:{}>".format(v, d[v])) for k, v in self.mana_symbols.items())
+        self.mana_pat = re.compile("|".join(mana_symbols.keys()))
 
-def mana_sub(text):
-    return mana_pat.sub(lambda a: mana_symbols[re.escape(a.group(0))] , text)
+    def mana_sub(self.text):
+        return mana_pat.sub(lambda a: mana_symbols[re.escape(a.group(0))] , text)
